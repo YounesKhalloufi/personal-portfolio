8625b23d81cebc26189f146752a09b01819af7ab
younes khalloufi <younes.khaloufi.1998@gmail.com>
2026-01-05 23:00:10 +0100

    change for all

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ console.log("javascript is working");
 document.getElementById("contactBtn").addEventListener("click", function () {
     alert("Thanks for contacting me!");
 });
+const themeBtn = document.getElementById("themeToggle");
+
+themeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeBtn.textContent = "Light Mode";
+    } else {
+        themeBtn.textContent = "Dark Mode";
+    }
+});
